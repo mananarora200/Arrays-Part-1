d4c525228262ab0d7f23b714b023d883b3e4de17
mananarora200 <mananarora200@gmail.com>
2020-08-06 16:50:13 +0530

    improved

--- duplicate in an array of N+1 integers.py
+++ duplicate in an array of N+1 integers.py
@@ -1,22 +1,19 @@
 import os
 os.system("cls")
-while 1:
-    num_array = []
-    flag = 0
-    count = int(input("Enter number of elements :"))
-    for i in range(count):
-        num_array.append(int(input(f"Enter {i+1} element :")))
-    print(num_array)
+def find(num_array):
+    num_array = sorted(num_array)
     for i,j in enumerate(num_array):
         try:    
             if j == num_array[i+1]:
-                flag = 1
-                break
+                return print(f"Repeated element is {j}")
         except:
             continue
-    if flag == 1:
-        print("There is an repeated element in this array")
-    else:
-        print("There is no repeated element in this array")
+    return print("There is no repeated element found in this array")
+while 1:
+    num_array = []
+    count = int(input("Enter number of elements :"))
+    for i in range(count):
+        num_array.append(int(input(f"Enter {i+1} element :")))
+    find(num_array)
     input("Press any key to continue ...")
     os.system("cls")
